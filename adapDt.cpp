5989fc02d0df71cbe1893eae5be96a7102405cc1
Mark Stock <markjstock@gmail.com>
2023-09-23 10:58:11 -0400

    Added routines for calculating jerk magnitude, stubs for disk initialization

--- adapDt.cpp
+++ adapDt.cpp
@@ -23,6 +23,12 @@
 #endif
 
 static float num_flops_kernel = 22.f;
+static float num_flops_accjerk = 23.f;
+static float num_flops_jerk = 17.f;
+
+//
+// kernels to compute accelerations on particles
+//
 
 // serial (x86) instructions
 template <class T>
@@ -61,7 +67,6 @@ inline void nbody_kernel(const float sx, const float sy, const float sz,
     *taz += factor * dz;
 }
 
-
 template <class T>
 void __attribute__ ((noinline)) nbody_serial(const int ns_start, const int ns_end,
                                      const T* const __restrict__ x,
@@ -84,6 +89,128 @@ void __attribute__ ((noinline)) nbody_serial(const int ns_start, const int ns_en
     }
 }
 
+//
+// kernels to compute accelerations and jerk magnitude on particles
+//
+
+// serial (x86) instructions
+template <class T>
+static inline void nbody_kernel_accjerk(const T sx, const T sy, const T sz,
+                                const T sm, const T sr,
+                                const T tx, const T ty, const T tz, const T tr2,
+                                T* const __restrict__ tax, T* const __restrict__ tay, T* const __restrict__ taz,
+                                T* const __restrict__ tjm) {
+    // 22 flops
+    const T dx = sx - tx;
+    const T dy = sy - ty;
+    const T dz = sz - tz;
+    const T r2 = dx*dx + dy*dy + dz*dz + sr*sr + tr2;
+    const T invR = 1.0 / sqrt(r2);
+    const T invR2 = invR * invR;
+    const T factor = sm * invR * invR2;
+    *tax += factor * dx;
+    *tay += factor * dy;
+    *taz += factor * dz;
+    *tjm += factor;
+}
+// specialize for float
+template <>
+inline void nbody_kernel_accjerk(const float sx, const float sy, const float sz,
+                         const float sm, const float sr,
+                         const float tx, const float ty, const float tz, const float tr2,
+                         float* const __restrict__ tax, float* const __restrict__ tay, float* const __restrict__ taz,
+                         float* const __restrict__ tjm) {
+    // 22 flops
+    const float dx = sx - tx;
+    const float dy = sy - ty;
+    const float dz = sz - tz;
+    const float r2 = dx*dx + dy*dy + dz*dz + sr*sr + tr2;
+    const float invR = 1.0f / sqrtf(r2);
+    const float invR2 = invR * invR;
+    const float factor = sm * invR * invR2;
+    *tax += factor * dx;
+    *tay += factor * dy;
+    *taz += factor * dz;
+    *tjm += factor;
+}
+
+template <class T>
+void __attribute__ ((noinline)) nbody_serial_accjerk(const int ns_start, const int ns_end,
+                                     const T* const __restrict__ x,
+                                     const T* const __restrict__ y,
+                                     const T* const __restrict__ z,
+                                     const T* const __restrict__ m,
+                                     const T* const __restrict__ r,
+                                     const int nt_start, const int nt_end,
+                                     T* const __restrict__ ax,
+                                     T* const __restrict__ ay,
+                                     T* const __restrict__ az,
+                                     T* const __restrict__ jm) {
+
+    #pragma omp parallel for
+    for (int i = nt_start; i < nt_end; i++) {
+        const T tr2 = r[i]*r[i];
+        for (int j = ns_start; j < ns_end; j++) {
+            nbody_kernel_accjerk<T>(x[j], y[j], z[j], m[j], r[j],
+                            x[i], y[i], z[i], tr2, &ax[i], &ay[i], &az[i], &jm[i]);
+        }
+    }
+}
+
+//
+// kernels to compute jerk magnitude on particles
+//
+
+// serial (x86) instructions
+template <class T>
+static inline void nbody_kernel_jerk(const T sx, const T sy, const T sz,
+                                const T sm, const T sr,
+                                const T tx, const T ty, const T tz, const T tr2,
+                                T* const __restrict__ tjm) {
+    // 17 flops
+    const T dx = sx - tx;
+    const T dy = sy - ty;
+    const T dz = sz - tz;
+    const T r2 = dx*dx + dy*dy + dz*dz + sr*sr + tr2;
+    const T invR = 1.0 / sqrt(r2);
+    const T invR2 = invR * invR;
+    *tjm += sm * invR * invR2;
+}
+// specialize for float
+template <>
+inline void nbody_kernel_jerk(const float sx, const float sy, const float sz,
+                         const float sm, const float sr,
+                         const float tx, const float ty, const float tz, const float tr2,
+                         float* const __restrict__ tjm) {
+    // 17 flops
+    const float dx = sx - tx;
+    const float dy = sy - ty;
+    const float dz = sz - tz;
+    const float r2 = dx*dx + dy*dy + dz*dz + sr*sr + tr2;
+    const float invR = 1.0f / sqrtf(r2);
+    const float invR2 = invR * invR;
+    *tjm += sm * invR * invR2;
+}
+
+template <class T>
+void __attribute__ ((noinline)) nbody_serial_jerk(const int ns_start, const int ns_end,
+                                     const T* const __restrict__ x,
+                                     const T* const __restrict__ y,
+                                     const T* const __restrict__ z,
+                                     const T* const __restrict__ m,
+                                     const T* const __restrict__ r,
+                                     const int nt_start, const int nt_end,
+                                     T* const __restrict__ jm) {
+
+    #pragma omp parallel for
+    for (int i = nt_start; i < nt_end; i++) {
+        const T tr2 = r[i]*r[i];
+        for (int j = ns_start; j < ns_end; j++) {
+            nbody_kernel_jerk<T>(x[j], y[j], z[j], m[j], r[j],
+                            x[i], y[i], z[i], tr2, &jm[i]);
+        }
+    }
+}
 
 // not really alignment, just minimum block sizes
 inline int buffer(const int _n, const int _align) {
@@ -242,11 +369,19 @@ struct Particles {
   }
 
   void init_rand(std::mt19937 _gen) {
-    s.init_rand(_gen);
     std::uniform_real_distribution<> zmean_mass(0.0, 1.0);
     for (int i=0; i<n; ++i) m[i] = zmean_mass(_gen);
     const T rad = 1.0 / std::sqrt((T)n);
     for (int i=0; i<n; ++i) r[i] = rad;
+    s.init_rand(_gen);
+  }
+
+  void init_disk(std::mt19937 _gen) {
+    std::uniform_real_distribution<> zmean_mass(0.0, 1.0);
+    for (int i=0; i<n; ++i) m[i] = zmean_mass(_gen);
+    const T rad = 1.0 / std::sqrt((T)n);
+    for (int i=0; i<n; ++i) r[i] = rad;
+    s.init_rand(_gen);
   }
 
   void deep_copy(const Particles& _from) {
@@ -437,9 +572,14 @@ void __attribute__ ((noinline)) exchange_sources(const Particles& sendsrcs, cons
 #endif
 
 static inline void usage() {
-    fprintf(stderr, "Usage: adapDt.bin [-n <number>] [-s <steps>]\n");
+    fprintf(stderr, "Usage: adapDt.bin [-n <number>] [-s <steps>] [-dt <num>] [-l <nlevels>] [-end <time>]\n");
     exit(1);
 }
+
+enum ParticleDistribution {
+  cube,
+  disk
+};
 
 // main program
 
@@ -458,6 +598,7 @@ int main(int argc, char *argv[]) {
     std::string outfile;
     std::string comparefile;
     std::string errorfile;
+    ParticleDistribution distro = cube;
 
     for (int i=1; i<argc; i++) {
         if (strncmp(argv[i], "-n=", 3) == 0) {
@@ -570,7 +711,13 @@ int main(int argc, char *argv[]) {
     // allocate fp64 and fp32 particle data
     Particles<double> orig(numSrcs);
     if (infile.empty()) {
-        orig.init_rand(gen);
+        if (distro = cube) {
+            // if random cloud and linear masses
+            orig.init_rand(gen);
+        } else {
+            // if disk distribution with power law masses
+            orig.init_disk(gen);
+        }
     } else {
         orig.fromfile(infile);
     }
